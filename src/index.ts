92d32d80f5195f5b34126811c332c0ebaf93d8ca
Adriano Tirloni <tirloni.adriano@gmail.com>
2026-04-09 05:57:57 -0300

    allow more exports / imports modes

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,10 @@
 export * as countries from './regexen/countries/_index.ts';
 export * as datetime from './regexen/datetime/_index.ts';
+
+import * as countries from './regexen/countries/_index.ts';
+import * as datetime from './regexen/datetime/_index.ts';
+
+const RegexBelt = { countries, datetime };
+
+export { RegexBelt };
+export default RegexBelt;
